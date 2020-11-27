58d2e41b18a61e05daf8f6483260fc2b6d66b454
mounaiban <29518031+mounaiban@users.noreply.github.com>
2020-11-27 23:46:32 +1100

    Add unit tests for TranslationDict

---
+++ tests/tests_translation_dict.py
@@ -0,0 +1,92 @@
+"""
+UILAAT TranslationDict class Tests
+
+"""
+# Copyright © 2020 Moses Chong
+#
+# This file is part of the UILAAT: The Unicode Interlingual Aesthetic
+# Appropriation Toolkit
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from unittest import TestCase
+from uilaat import TranslationDict, SUBPOINT
+
+# Test Resources
+FANCY_A = '\u1555'
+FANCY_Bm = '\u18b5\u15ff'
+DEFAULT_OUT = SUBPOINT + '\u20e0'  # put circle-slash over chars
+dict_plain = {'a':FANCY_A, 'b':FANCY_Bm,}
+dict_with_default = { 'a':FANCY_A, 'b':FANCY_Bm, '':DEFAULT_OUT,}
+dict_with_none_default = { 'a':FANCY_A, 'b':FANCY_Bm, '':None,}
+
+class TranslationDictGetitemTests(TestCase):
+    """
+    Tests to verify correctness of lookup output of TranslationDict
+    """
+    clsc = TranslationDict      # clsc => candidate class
+
+    def test_getitem(self):
+        """
+        Look up chars found in dict
+        """
+        tdict_a = self.clsc.from_dict(dict_plain)   # tdict => test dict
+        out_single = tdict_a[ord('a')]
+        out_multi = tdict_a[ord('b')]
+        
+        self.assertEqual(out_single, FANCY_A)
+        self.assertEqual(out_multi, FANCY_Bm)
+
+    def test_getitem_not_found(self):
+        """
+        Look up chars _not found in dict
+        """
+        tdict_a = self.clsc.from_dict(dict_plain)
+        out_nf = tdict_a[ord('\uffff')]
+        self.assertEqual(out_nf, '\uffff')
+        # PROTIP: Unicode non-characters can be used for testing 
+
+    def test_getitem_default(self):
+        """
+        Look up chars found in dict with default output
+        """
+        tdict_d = self.clsc.from_dict(dict_with_default)
+        out_single = tdict_d[ord('a')]
+        out_multi = tdict_d[ord('b')]
+
+        self.assertEqual(out_single, FANCY_A)
+        self.assertEqual(out_multi, FANCY_Bm)
+
+    def test_getitem_not_found_default(self):
+        """
+        Look up chars _not found in dict, but with default output
+        """
+        test_char = '\uffff'
+        tdict_d = self.clsc.from_dict(dict_with_default)
+        out_nf = tdict_d[ord(test_char)]
+        out_expected = DEFAULT_OUT.replace(SUBPOINT, test_char)
+
+        self.assertEqual(out_nf, out_expected)
+
+    def test_getitem_none_default(self):
+        """
+        Look up chars _not found in dict, but with None as default output
+        """
+        test_char = '\uffff'
+        tdict_d = self.clsc.from_dict(dict_with_none_default)
+        out_nf = tdict_d[ord(test_char)]
+
+        self.assertEqual(out_nf, None)
+
